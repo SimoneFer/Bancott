61827ff900426f64380d19ee45162fce33a493a5
SimoneFer <simonewica@bol.com.br>
2019-08-31 17:28:15 -0300

    Aula5

--- src/main/java/br/com/tt/bancott/model/Correntista.java
+++ src/main/java/br/com/tt/bancott/model/Correntista.java
@@ -1,27 +1,26 @@
 package br.com.tt.bancott.model;
 
 public abstract class Correntista {
-	
+
 	private String nome;
 	private Conta conta;
-	
-	public Correntista (String nome, Conta conta) {
+
+	public Correntista(String nome, Conta conta) {
 		this.nome = nome;
 		this.conta = conta;
 	}
+
 	@Override
 	public String toString() {
-		return String.format("Correntista = [nome=%s,conta = %s]",
-				nome,
-				conta);
+		return String.format("Correntista = [nome=%s,conta = %s]", nome, conta);
 	}
+
 	public void incluirMovimentoAConta(Movimento movimento) {
 		this.conta.incluirMovimento(movimento);
 	}
-	
-		public Movimento[] listarMovimentosDaConta() {
+
+	public Movimento[] listarMovimentosDaConta() {
 		return conta.getMovimentos();
-		
-		
+
 	}
 }
